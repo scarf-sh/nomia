d03215dcf631d5a147136a6a4a41ed9fab830ade
Shea Levy <shea.levy@scarf.sh>
2021-04-21 07:14:08 -0400

    Reference a bnfc-generated name grammar in the docs

---
+++ shell.nix
@@ -0,0 +1,11 @@
+let
+  sources = import ./nix/sources.nix { };
+
+  pkgs = import sources.nixpkgs {};
+
+  texlive = pkgs.texlive.combine { inherit (pkgs.texlive) scheme-small ucs; };
+in pkgs.mkShell {
+  name = "nomia-dev-env";
+
+  nativeBuildInputs = [ pkgs.haskellPackages.BNFC texlive ];
+}
