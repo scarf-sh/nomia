3441ddb17bb7d51d8bbe8f86b1b958635faa318b
Shea Levy <shea.levy@scarf.sh>
2021-04-26 19:31:05 -0400

    core: Rustdoc for nomia_core::name.

--- core/src/name/mod.rs
+++ core/src/name/mod.rs
@@ -1,26 +1,52 @@
+//! Basic AST for names.
+
 use std::fmt;
 use std::iter::IntoIterator;
 
+/// Type parameters for the [Name](Name) AST.
+///
+/// Implementing this trait allows the user to use alternative types
+/// of references or sequences.
+///
+/// By default, you should just used [OwnedName](OwnedName).
 pub trait NameParameters {
+    /// A reference to a node declaration, held in the top-level [Name](Name).
     type DeclarationRef: AsRef<Declaration<Self>>;
+    /// An iterable sequence of [DeclarationRefs](Self::DeclarationRef).
     type Declarations: IntoIterator<Item = Self::DeclarationRef>;
 
+    /// A reference to another [Name](Name) composed into the [Name](Name) being defined.
     type NestedName: AsRef<Name<Self>>;
 
+    /// A reference to a [Substitution](Substitution) nested under another [Substitution](Substitution).
+    ///
+    /// For example, in `foo(bar: baz(qux: quux))`, `baz(qux: quux)` is held as a `SubstitutionRef`.
     type SubstitutionRef: AsRef<Substitution<Self>>;
 
+    /// An iterable sequence of [SubstitutionSpecs](SubstitutionSpec), the inputs to a [Substitution](Substitution).
     type SubstitutionSpecs: IntoIterator<Item = SubstitutionSpec<Self>>;
 
+    /// Key names in a parameter list.
     type ParameterName: AsRef<str>;
+    // TODO Multiple types
+    /// Values in a parameter list.
     type ParameterValue: AsRef<str>;
     // TODO Should this be a map-like interface instead of a list of kv pairs?
+    /// An iterable sequence of key-value [Parameters](Parameter).
     type Parameters: IntoIterator<Item = Parameter<Self>>;
 
+    /// An identifier for variables and name/namespace IDs.
     type Identifier: AsRef<str>;
 }
 
+/// A Nomia name
+///
+/// In fully generic form, `Name`s consist of a non-empty sequence of
+/// node [Declarations](Declaration).
 pub struct Name<P: NameParameters + ?Sized> {
+    /// The (possibly empty) initial sequence of [Declarations](Declaration).
     pub first_declarations: P::Declarations,
+    /// The last [Declaration](Declaration) in the sequence.
     pub last_declaration: P::DeclarationRef,
 }
 
@@ -57,8 +83,18 @@ where
 {
 }
 
+/// A node declaration in a composition graph.
+///
+/// Each `Declaration` defines a name and any names substituted into
+/// it.
+///
+/// The substituted name can optionally be assigned to a specified
+/// variable.
 pub struct Declaration<P: NameParameters + ?Sized> {
+    /// The variable name to bind to the substituted name,
+    /// if any.
     pub var: Option<Identifier<P>>,
+    /// The name and its substituted inputs.
     pub val: Substitution<P>,
 }
 
@@ -104,8 +140,11 @@ where
 {
 }
 
+/// A name together with any inputs substituted into it.
 pub struct Substitution<P: NameParameters + ?Sized> {
+    /// An identification of the name to substitute into.
     pub name: NameRef<P>,
+    /// The inputs to substitute into the given name.
     pub inputs: P::SubstitutionSpecs,
 }
 
@@ -151,13 +190,29 @@ where
 {
 }
 
+/// An identification of a [Name](Name).
 pub enum NameRef<P: NameParameters + ?Sized> {
+    /// An atomic name, with no substitutions.
     Atomic {
+        /// The namespace the name is in.
+        ///
+        /// If omitted, it must be filled in from the context this
+        /// name is resolved in.
         namespace_id: Option<NamespaceId<P>>,
+        /// The specific name within the namespace.
         name_id: ParameterizedId<P>,
     },
+    /// A variable.
+    ///
+    /// The variable must be bound somewhere within the scope of
+    /// the usage.
     Variable(Identifier<P>),
+    /// A resolved name reference.
+    ///
+    /// The variable name must be bound to an appropriate resolved
+    /// name when resolving the name it is referenced within.
     Resolved(Identifier<P>),
+    /// A recursively inlined other name.
     Nested(P::NestedName),
 }
 
@@ -217,8 +272,13 @@ where
 {
 }
 
+/// A specification of an input to be substituted.
 pub struct SubstitutionSpec<P: NameParameters + ?Sized> {
+    /// The name of the input to substitute into.
+    ///
+    /// If omitted, the input must be determinable positionally.
     pub input_id: Option<Identifier<P>>,
+    /// The name output to substitute into the given input.
     pub input_val: OutputRef<P>,
 }
 
@@ -255,8 +315,15 @@ where
 {
 }
 
+/// A reference to an output of some name.
 pub struct OutputRef<P: NameParameters + ?Sized> {
+    /// The name whose output is being referenced.
     pub name: P::SubstitutionRef,
+    /// The specific output being referenced.
+    ///
+    /// If omitted, the output ID must be inferrable from the
+    /// resolution context (either because there is only one or through
+    /// a default output ID).
     pub output_id: Option<Identifier<P>>,
 }
 
@@ -293,8 +360,12 @@ where
 {
 }
 
+/// An identification of a namespace within a root namespace of
+/// namespaces.
 pub enum NamespaceId<P: NameParameters + ?Sized> {
+    /// A namespace identified by an opaque ID and parameters.
     Builtin(ParameterizedId<P>),
+    /// A namespace identified as the output of some other name.
     Named(OutputRef<P>),
 }
 
@@ -334,8 +405,11 @@ where
 {
 }
 
+/// An identifier paired with a parameter list.
 pub struct ParameterizedId<P: NameParameters + ?Sized> {
+    /// The opaque identifier.
     id: Identifier<P>,
+    /// The parameters modifying the identifier.
     params: P::Parameters,
 }
 
@@ -372,8 +446,11 @@ where
 {
 }
 
+/// A key-value pair parameterizing some identifier.
 pub struct Parameter<P: NameParameters + ?Sized> {
+    /// The parameter name.
     pub name: P::ParameterName,
+    /// The parameter value.
     pub val: P::ParameterValue,
 }
 
@@ -410,6 +487,7 @@ where
 {
 }
 
+/// An opaque identifier.
 pub struct Identifier<P: NameParameters + ?Sized>(pub P::Identifier);
 
 impl<P: NameParameters + ?Sized> fmt::Debug for Identifier<P>
@@ -432,6 +510,8 @@ where
 
 impl<P: NameParameters + ?Sized> Copy for Identifier<P> where P::Identifier: Copy {}
 
+/// A type to carry [NameParameters](NameParameters) using [Box](Box)
+/// for references and [Vec](Vec) for sequences.
 pub enum OwnedNameParameters {}
 
 impl NameParameters for OwnedNameParameters {
@@ -451,4 +531,7 @@ impl NameParameters for OwnedNameParameters {
     type Identifier = String;
 }
 
+/// A [Name](Name) where all internal references and sequences are owned.
+///
+/// Built on top of [Box](Box) and [Vec](Vec).
 pub type OwnedName = Name<OwnedNameParameters>;
